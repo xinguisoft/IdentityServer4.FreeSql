4fe241955698b866b3a14397d4bd132f5d64ee88
sunrise <li986666@126.com>
2021-02-26 17:20:09 +0800

    修改版本为1.1.1，增加生成IdentityServer4.FreeSql下的实体表信息方法。

---
+++ src/IdentityServer4.FreeSql/Extensions/FreeSqlExtensions.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityServer4.FreeSql.Extensions
+{
+    public static class FreeSqlExtensions
+    {
+        /// <summary>
+        /// 创建IdentityServer4.FreeSql下的实体表信息
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="freeSql"></param>
+        /// <returns></returns>
+        public static async Task CreateEntitiesTable<T>(this IFreeSql<T> freeSql)
+        {
+            try
+            {
+                await Task.Run(() =>
+                {
+                    freeSql.CodeFirst.SyncStructure(GetTypes());
+                });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"CreateTable:{ex.Message}");
+            }
+        }
+
+        /// <summary>
+        /// 创建IdentityServer4.FreeSql下的实体表信息
+        /// </summary>
+        /// <param name="freeSql"></param>
+        /// <returns></returns>
+        public static async Task CreateEntitiesTable(this IFreeSql freeSql)
+        {
+            try
+            {
+                await Task.Run(() =>
+                {
+                    freeSql.CodeFirst.SyncStructure(GetTypes());
+                });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"CreateTable:{ex.Message}");
+            }
+        }
+
+        /// <summary>
+        /// 查找实体类信息
+        /// </summary>
+        /// <returns></returns>
+        private static Type[] GetTypes()
+        {
+            try
+            {
+                List<Type> tableAssembies = new List<Type>();
+                List<string> entitiesFullName = new List<string>()
+                    {
+                    "IdentityServer4.FreeSql.Entities",
+                    };
+
+                var types = Assembly.GetAssembly(typeof(FreeSqlExtensions)).GetExportedTypes();
+                foreach (Type type in types)
+                {
+                    foreach (var fullname in entitiesFullName)
+                    {
+                        if (type.FullName.StartsWith(fullname) && type.IsClass && !type.IsAbstract)
+                        {
+                            tableAssembies.Add(type);
+                        }
+                    }
+                }
+                return tableAssembies.ToArray();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
--- test/IdentityServer4.FreeSql.MSSqlTest/Startup.cs
+++ test/IdentityServer4.FreeSql.MSSqlTest/Startup.cs
@@ -10,7 +10,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-//using IdentityServer4.FreeSql;
 
 namespace IdentityServer4.FreeSql.MSSqlTest
 {
